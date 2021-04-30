ad5bb6dd561773e8ce78127036b5c563ac6535ff
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-04-30 14:30:38 -0300

    Contact Model, Get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,26 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET  api/contacts
 // @desc    Get all users Contacts
 // @access  Private
 
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
-
 
 // @route   POST  api/contacts
 // @desc    Add new Contact
@@ -19,7 +30,6 @@ router.post('/', (req, res) => {
   res.send('Add Contact');
 });
 
-
 // @route   PUT  api/contacts/:id
 // @desc    Update Contact
 // @access  Private
@@ -27,7 +37,6 @@ router.post('/', (req, res) => {
 router.put('/', (req, res) => {
   res.send('Update Contact');
 });
-
 
 // @route   Delete  api/contacts/:id
 // @desc    Delete Contact
