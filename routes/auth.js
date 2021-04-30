0d209e8f8b9e5fe49e4060fcd0749b9b9ddecf0b
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-04-30 14:08:33 -0300

    auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -14,10 +14,12 @@ const router = express.Router();
 // @access  Private
 
 router.get('/', auth, async (req, res) => {
-  try{
-    const user = await User.findById(req.user.id).select
-  } catch(err){
-
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
