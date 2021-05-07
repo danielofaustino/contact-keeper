c45178871faebbbd04e9cf9ee1a2f857ae247121
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-05-07 16:16:03 -0300

    Update contacts to database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -58,12 +58,38 @@ const ContactState = (props) => {
   };
 
   // DElete Contact
-  const deleteContact = async id => {
+  const deleteContact = async (id) => {
     try {
       await axios.delete(`/api/contacts/${id}`);
       dispatch({
         type: DELETE_CONTACT,
         payload: id,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data,
       });
     } catch (err) {
       dispatch({
@@ -86,11 +112,6 @@ const ContactState = (props) => {
   // Clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // Filter Contacts
