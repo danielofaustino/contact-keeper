81d1a0f3a6680fdc79177cc2694249e7ed3b3949
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-05-07 13:39:05 -0300

    Adding contact to database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -12,44 +12,30 @@ import {
   CLEAR_FILTER,
   SET_ALERT,
   REMOVE_ALERT,
+  CONTACT_ERROR,
 } from '../types';
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: 'Daniel Faustino',
-        email: 'danieloliveirafaustino@gmail.com',
-        phone: '94909-1999',
-        type: 'personal',
-      },
-      {
-        id: 2,
-        name: 'Isabel Faustino',
-        email: 'isabelfausitno@gmail.com',
-        phone: '94909-1999',
-        type: 'professional',
-      },
-      {
-        id: 3,
-        name: 'Fabrine Macedo',
-        email: 'fabrinemacedo@gmail.com',
-        phone: '94909-1999',
-        type: 'personal',
-      },
-    ],
+    contacts: [],
     current: null,
     filtered: null,
+    error: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
 
-  const addContact = (contact) => {
-    contact.id = uuidv4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async (contact) => {
+    const config = { headers: { 'Content-Type': 'apllication/json' } };
+
+    try {
+      const res = await axios.post('/api/contacts', contact, config);
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   // DElete Contact
@@ -88,6 +74,7 @@ const ContactState = (props) => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -8,6 +8,7 @@ import {
   CLEAR_FILTER,
   SET_ALERT,
   REMOVE_ALERT,
+  CONTACT_ERROR,
 } from '../types';
 
 export default (state, action) => {
@@ -54,6 +55,11 @@ export default (state, action) => {
       return {
         ...state,
         filtered: null,
+      };
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
